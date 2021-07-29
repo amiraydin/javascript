d28d0d5e5a4b360d1c57836d5839c0f3dbeee2a1
amiraydin <amiraydin@outlook.fr>
2021-07-29 16:36:46 +0200

    modification of day 8

--- jour8/jour8.js
+++ jour8/jour8.js
@@ -21,3 +21,73 @@ function getCountries() {
 getCountries()
 
 
+
+//  ex2
+function getFact() {
+    request.get("https://api.chucknorris.io/jokes/random", function(error, response, body) {
+        console.log(response.statusCode);
+        var fact = JSON.parse(body);
+        console.log(fact.value);
+    });
+}
+
+
+// ex3
+function catchPokemon(id, lang) {
+    request.get(`https://pokeapi.co/api/v2/pokemon-species/${id}`, function(error, response, body) {
+        console.log(response.statusCode);
+        var pokemon = JSON.parse(body);
+        var translatedName = pokemon.names.find(function(element) {
+            return element.language.name === lang;
+        })
+        console.log(`Mon pokemon numéro ${pokemon.id} s'appelle: ${translatedName.name}`);
+    })
+}
+
+// ex4 
+
+var prompt = require("prompt");
+
+prompt.start();
+var schema = {
+  properties: {
+    action: {
+      description: "do you want to draw a card or pass?",
+      required: true,
+    },
+  },
+};
+
+
+var max = 21;
+var min = 16;
+var bankCards = Math.floor(Math.random() * (max - min) + min);
+var playerCards = 0;
+
+function play() {
+    prompt.get(schema, function(error, result) {
+        if (result.action === "draw") {
+            var max = 10;
+            var min = 1;
+            playerCards += Math.floor(Math.random() * (max - min) + min);
+            console.log(`Your card worth: ${playerCards}`);
+            if (playerCards > 21) {
+                console.log("You loose");
+            } else {
+                play();
+            }
+        } else {
+            console.log(`The bank had ${bankCards}`);
+            if (playerCards === 21) {
+                console.log("Black Jack, you win");
+            } else if (playerCards > bankCards) {
+                console.log("You win");
+            } else {
+                console.log("You loose");
+            }
+        }
+    })
+}
+
+play();
+
